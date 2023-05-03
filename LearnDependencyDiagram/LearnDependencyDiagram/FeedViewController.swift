0ae04cea74b5395149fd33428101899196036cc3
Galih Samodra Wicaksono <>
2023-05-03 09:57:59 +0700

    add(FeedViewController): add FeedViewController

---
+++ LearnDependencyDiagram/LearnDependencyDiagram/FeedViewController.swift
@@ -0,0 +1,10 @@
+//
+//  FeedViewController.swift
+//  LearnDependencyDiagram
+//
+//  Created by Galih Samodra Wicaksono on 03/05/23.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {}
