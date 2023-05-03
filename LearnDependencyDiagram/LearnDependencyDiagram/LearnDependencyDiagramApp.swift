bd11cc7a9ee2bd9a26824b9a3c216a2689481446
Galih Samodra Wicaksono <>
2023-05-03 09:54:06 +0700

    wip initial push

---
+++ LearnDependencyDiagram/LearnDependencyDiagram/LearnDependencyDiagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnDependencyDiagramApp.swift
+//  LearnDependencyDiagram
+//
+//  Created by Galih Samodra Wicaksono on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnDependencyDiagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
